e1582954c9b732c4f7b62e7e076b53261ad2f1f2
Anders Mogensen <am@mapspeople.com>
2019-09-11 08:26:05 +0200

    Prepare resouces.

--- mapsindoors.custom-element.js
+++ mapsindoors.custom-element.js
@@ -1,5 +1,32 @@
 export default class extends HTMLElement {
     constructor() {
         super();
+
+        const template = document.createElement('template');
+
+        template.innerHTML = '<div><p>Loading...</p></div>';
+
+        this.attachShadow({ mode: 'open' });
+        this.shadowRoot.appendChild(template.content.cloneNode(true));
+
+        this.googleApiKey = this.getAttribute('google-api-key');
+        this.mapsIndoorsSolutionId = this.getAttribute('solution-id');
+    }
+
+    async connectedCallback() {
+        await this.injectScript(`//maps.googleapis.com/maps/api/js?v=3&key=${this.googleApiKey}&libraries=geometry,places`);
+        await this.injectScript(`https://app.mapsindoors.com/mapsindoors/js/sdk/mapsindoors-3.4.0.js.gz?apikey=${this.mapsIndoorsSolutionId}`);
+    }
+
+    /* ------------------------------------------------------------------------- */
+
+    injectScript(url) {
+        return new Promise(resolve => {
+            const scriptTag = document.createElement('script');
+            scriptTag.setAttribute('type', 'text/javascript');
+            scriptTag.setAttribute('src', url);
+            document.body.appendChild(scriptTag);
+            scriptTag.onload = () => resolve();
+        });
     }
 };
